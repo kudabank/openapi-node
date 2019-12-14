be8be9751092b3ae51beb7c5245b3a80ea4c2fe0
AbdulSamii Ajala <jalasem@users.noreply.github.com>
2019-12-14 22:58:08 +0100

    fix: privateKey and publicKey buffer to string

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ const { RSAEncrypt, RSADecrypt } = require('./algorithms/rsa')
 function Kuda (param) {
   if (!param) return console.log('Error: publicKey, privateKey, clientKey are required!')
 
-  const { publicKey, privateKey, clientKey } = param
+  let { publicKey, privateKey } = param
+  publicKey = publicKey.toString()
+  privateKey = privateKey.toString()
+
+  const { clientKey } = param
+  
+  if (!publicKey || !privateKey || !clientKey) return console.log('Error: publicKey, privateKey, clientKey are required!')
+  
   const password = `${clientKey}-${shortid.generate().substring(0, 5)}`
 
   /**
